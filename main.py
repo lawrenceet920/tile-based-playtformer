67dbf7b54f9e94247c0154d6dfc539225974fdb9
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2025-05-19 13:20:46 -0400

    start

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
 # Ethan Lawrence
-# May 16 2025
-# Sprites
+# May 19 2025
+# Tile Based Platformer
 
 import random
 import pygame
 pygame.init()
 
+# Global Constants
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 1000
+
+# Images
 class Game:
     '''Game Logic'''
     def __init__(self):
         self.shapes = pygame.sprite.Group()
+        self.backround_objects = pygame.sprite.Group()
+
         self.running = True
-        self.screen = pygame.display.set_mode((600, 600))
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.set_caption('sprite test')
         self.clock = pygame.time.Clock()
         self.count = 0
         self.main()
     def main(self):
+        self.backround_objects.add(Backround_object((150, 150), 'cloud.png', (17, 193, 242)))
+        self.backround_objects.add(Backround_object((100, 150), 'cloud.png', (17, 193, 242)))
+        self.backround_objects.add(Backround_object((100, 100), 'cloud.png', (17, 193, 242)))
+        self.backround_objects.add(Backround_object((100, 100), 'sun.png', (255, 255, 255)))
         while self.running:
+            self.clock.tick(60)
             self.handle_events()
             self.update()
             self.draw()
-            self.clock.tick(60)
     def handle_events(self):
         '''Checks if user quits game'''
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                pygame.quit
                 self.running = False
     def draw(self):
         self.screen.fill((0,0,255))
         self.shapes.draw(self.screen)
+        self.backround_objects.draw(self.screen)
         pygame.display.flip()
     def update(self):
         self.shapes.update()
@@ -38,9 +51,8 @@ class Game:
             self.count = 0
             # keys = pygame.key.get_pressed()
             # if keys[pygame.K_SPACE]:
-            self.shapes.add(some_shape((300,300), (random.randrange(0,255),random.randrange(0,255), random.randrange(0,255)), (10,10), (100, 50), random.randint(0,1)))
-
-class some_shape(pygame.sprite.Sprite):
+            self.shapes.add(Some_shape((300,300), (random.randrange(0,255),random.randrange(0,255), random.randrange(0,255)), (10,10), (100, 50), random.randint(0,1)))
+class Some_shape(pygame.sprite.Sprite):
     def __init__(self, pos, color, speed, scale, shape):
         super().__init__()
         self.x = pos[0]
@@ -60,6 +72,14 @@ class some_shape(pygame.sprite.Sprite):
     def update(self):
         self.x += self.vel[0]
         self.y += self.vel[1]
+        self.rect = (self.x, self.y)
+class Backround_object(pygame.sprite.Sprite):
+    def __init__(self, pos, image, color_key):
+        super().__init__()
+        self.x = pos[0]
+        self.y = pos[1]
+        self.image = pygame.image.load(image)
+        self.image.set_colorkey(color_key)
         self.rect = (self.x, self.y)
 
 rungame = Game()
